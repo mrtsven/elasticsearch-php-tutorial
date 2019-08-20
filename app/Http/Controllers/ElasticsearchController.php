5bdfb92227dfc3b116a54ce6e6a391717ac60f72
mrtsven <zahharov@hotmail.com>
2019-08-20 15:17:51 +0200

    Setup | Constructor with Elasticsearch/ClientBuilder

--- app/Http/Controllers/ElasticsearchController.php
+++ app/Http/Controllers/ElasticsearchController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Elasticsearch\ClientBuilder;
 use Illuminate\Http\Request;
 
 class ElasticsearchController extends Controller
 {
-    //
+    /**
+     * Our PHP Elasticsearch client
+     *
+     * @var Elasticsearch\ClientBuilder
+     */
+    protected $elasticSearchClient;
+    protected $hosts = ['localhost:9200'];
+
+    /**
+     * Let's construct our ClientBuilder beforehand so that we can easily call it in the future.
+     */
+    public function __construct()
+    {
+        $this->elasticSearchClient = ClientBuilder::create()->setHosts($this->hosts)->build();
+    }
 }
